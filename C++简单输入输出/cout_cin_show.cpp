7d6a2a25a3204a575faa85f7b3de647f75ec2135
crrrush <90398252+crrrush@users.noreply.github.com>
2022-09-21 12:27:54 +0800

    blog showing

---
+++ C++简单输入输出/cout_cin_show.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+
+//using namespace std;
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+	int a = 0;
+	float b = 0.0;
+	char c = 0;
+
+	cin >> a >> b >> c;
+
+	cout << endl;
+
+	cout << a  << endl;
+	cout << b << endl;
+	cout << c << endl;
+
+	return 0;
+}
